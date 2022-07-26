fa236882a73ff72c64888fd5adcaed3594b81ed4
tuan <tuanprokt445@gmail.com>
2022-07-26 16:53:50 +0700

    a

--- app/src/main/java/com/example/demoproject/BlankFragment2.java
+++ app/src/main/java/com/example/demoproject/BlankFragment2.java
@@ -1,15 +1,20 @@
 package com.example.demoproject;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.app.WallpaperManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.demoproject.databinding.FragmentBlank2Binding;
+
+import java.util.Calendar;
 
 
 public class BlankFragment2 extends Fragment {
@@ -22,9 +27,22 @@ public class BlankFragment2 extends Fragment {
         // Inflate the layout for this fragment
         FragmentBlank2Binding binding = FragmentBlank2Binding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                Toast.makeText(getActivity(), year + "/ " + month + "/" + day, Toast.LENGTH_SHORT).show();
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, day);
+            }
+        };
         binding.btnPreviewFragment.setOnClickListener(i -> {
             binding.imageView.setImageResource(R.drawable.astronaut);
+            new DatePickerDialog(getActivity(), dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)
+                    , calendar.get(Calendar.DAY_OF_MONTH)).show();
         });
+
         binding.btnChangeFragment.setOnClickListener(i -> {
             CustomViewDialog customViewDialog = new CustomViewDialog();
             customViewDialog.showDialog(getActivity(), "This is a normal message for a normal custom dialog");
@@ -37,4 +55,5 @@ public class BlankFragment2 extends Fragment {
         });
         return view;
     }
+
 }
