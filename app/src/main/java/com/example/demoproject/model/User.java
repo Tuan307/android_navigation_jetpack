ee418bbb3b7faaed19f54d2569208294172c4315
Pham Tuan <tuanprokt44@gmail.com>
2023-03-19 21:55:31 +0700

    aaaa

--- app/src/main/java/com/example/demoproject/model/User.java
+++ app/src/main/java/com/example/demoproject/model/User.java
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey;
 
 @Entity(tableName = "user_database")
 //hahahahaha
-//aaaaaaa
+//aaaaaaaaaa
 public class User {
     @PrimaryKey(autoGenerate = true)
     private int id;
